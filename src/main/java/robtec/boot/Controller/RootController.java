3e0877a75c5d69b46af110ed5396002af4c2f178
Rob Powell <rob.p.tec@gmail.com>
2014-12-20 19:27:40 +0000

    Added example controller mapped to root page

---
+++ src/main/java/robtec/boot/Controller/RootController.java
@@ -0,0 +1,13 @@
+package robtec.boot.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello from a Controller";
+    }
+}
